32b174873d67ec747ffb78f5a29367c977e7052e
Valerie R <vrosenhauer@gmail.com>
2024-01-02 20:23:46 -0500

    fixed bug in Person.js

--- part-3/App.js
+++ part-3/App.js
@@ -1,21 +1,21 @@
 function App() {
-    return (
-        <div>
-            <Person
-            name="Brian"
-            age={55}
-            hobbies={["bowling", "watching tv", "drinking beer"]}
-            />
-            <Person 
-            name="Sarah"
-            age={88}
-            hobbies={["knitting", "pinochle", "rollerblading"]}
-            />
-            <Person
-            name="Brooke"
-            age={34}
-            hobbies={["cross-stitching", "plumbing", "sticker making"]}
-            />
-        </div>
-    )''
+  return (
+    <div>
+      <Person
+        name="Brian"
+        age={55}
+        hobbies={["bowling", "watching tv", "drinking beer"]}
+      />
+      <Person
+        name="Sarah"
+        age={88}
+        hobbies={["knitting", "pinochle", "rollerblading"]}
+      />
+      <Person
+        name="Brooke"
+        age={34}
+        hobbies={["cross-stitching", "plumbing", "sticker making"]}
+      />
+    </div>
+  );
 }
--- part-3/Person.js
+++ part-3/Person.js
@@ -1,13 +1,23 @@
 const MAX_NAME_LENGTH_TO_SHOW = 6;
 
+/** Info on a single person.
+ *
+ * Props:
+ * - name
+ * - age
+ * - hobbies: array, like ["cats", "bridge", "fighting the patriarchy"]
+ */
+
 function Person({ age, hobbies, name }) {
   const voteText = age >= 18 ? "Go vote!" : "Go study!";
 
-  const hobbieLIs = hobbies.map((hobby) => <li>{hobby}</li>);
+  // your browser will issue a stern warning about this; we'll learn how to fix
+  // that later.
+  const hobbiesLIs = hobbies.map((hobby) => <li>{hobby}</li>);
 
   return (
     <div>
-      <p>Learn some new information about this person:</p>
+      <p>Learn some information about this person:</p>
       <ul>
         <li>Name: {name.slice(0, MAX_NAME_LENGTH_TO_SHOW)}</li>
         <li>Age: {age}</li>
